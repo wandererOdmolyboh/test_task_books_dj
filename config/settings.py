e3bde48ca827a62ddb75614ac2b53f59da79c9ac
dmolyboh <denys.molyboha@cosmonova.net>
2024-08-17 17:03:45 +0300

    append swagger

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
     # libraries
     'rest_framework',
+    'drf_yasg',
 
     # apps
     'library',
--- config/urls.py
+++ config/urls.py
@@ -14,12 +14,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_yasg import openapi
 from django.contrib import admin
-from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from django.urls import path, include, re_path
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Library API",
+      default_version='v1',
+      description="API for managing books in a library",
+   ),
+   public=True,
+)
 
 urlpatterns = [
     # application admin
     path('admin/', admin.site.urls),
+    # documentation
+    re_path(r'^docs/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
     # application library
     path('', include("library.urls", namespace="book")),
