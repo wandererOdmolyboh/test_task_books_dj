7a4c2df59b94991478501dfc7f643b4345495282
dmolyboh <denys.molyboha@cosmonova.net>
2024-08-17 13:09:24 +0300

    add serialize + views

--- library/views.py
+++ library/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import viewsets, filters
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework.pagination import PageNumberPagination
 
-# Create your views here.
+
+class BookPagination(PageNumberPagination):
+    page_size = 10
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    pagination_class = BookPagination
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['author', 'published_date', 'language']
