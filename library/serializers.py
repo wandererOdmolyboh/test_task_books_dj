ba5f1c151d88b687fa2b7ff7972d1e4748a02c45
dmolyboh <denys.molyboha@cosmonova.net>
2024-08-17 19:47:00 +0300

    append documentation swager

--- library/serializers.py
+++ library/serializers.py
@@ -3,6 +3,15 @@ from .models import Book
 
 
 class BookSerializer(serializers.ModelSerializer):
+    """
+    BookSerializer is a ModelSerializer for the Book model.
+
+    This serializer automatically creates fields that correspond to the Book model fields.
+    It includes all fields from the Book model ('__all__').
+
+    When creating a new book or updating an existing one using the API, this serializer
+    validates the data to ensure it fits the constraints of the Book model.
+    """
     class Meta:
         model = Book
         fields = '__all__'
--- library/views.py
+++ library/views.py
@@ -5,11 +5,25 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class BookPagination(PageNumberPagination):
+    """
+    BookPagination is a custom pagination class that inherits from PageNumberPagination.
+
+    It sets the default page size to 10 and allows the page size to be changed by a query parameter.
+    """
     page_size = 10
     page_size_query_param = 'page_size'
 
 
 class BookViewSet(viewsets.ModelViewSet):
+    """
+    BookViewSet is a viewset for the Book model.
+
+    It uses the BookSerializer to serialize and deserialize the Book model instances.
+    It uses the BookPagination class to paginate the results.
+    It allows filtering the results by the 'author', 'published_date', and 'language' fields.
+
+    The queryset includes all Book instances, ordered by their 'id'.
+    """
     queryset = Book.objects.all().order_by('id')
     serializer_class = BookSerializer
     pagination_class = BookPagination
